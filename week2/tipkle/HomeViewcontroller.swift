119ef65293970bc595a0ec914fa12fd164d92089
Yuri Lee <leeyuri.andapp@gmail.com>
2021-09-11 01:48:26 +0900

    어려워 ios~

--- week2/tipkle/HomeViewcontroller.swift
+++ week2/tipkle/HomeViewcontroller.swift
@@ -7,10 +7,22 @@
 
 import UIKit
 
+
 class HomeViewcontroller: UIViewController {
 
+    @IBOutlet weak var btnWrite: UIButton!
+    @IBAction func writeTip(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let logo = UIImage(named: "logo.png")
+        let imageView = UIImageView(image:logo)
+        self.navigationItem.titleView = imageView
+        
+        //let button = UIButton()
 
         // Do any additional setup after loading the view.
     }
