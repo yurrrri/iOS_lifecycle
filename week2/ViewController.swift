97cfc5f800c6ce26185a43db0783902d364e8591
Yuri Lee <leeyuri.andapp@gmail.com>
2021-09-28 06:47:38 +0900

    commit

--- week2/ViewController.swift
+++ week2/ViewController.swift
@@ -9,22 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
+
     @IBOutlet weak var propertyTextField: UITextField!
     @IBAction func sendData(_ sender: Any) {
         //네비게이션 뷰 컨트롤러로 데이터를 전달하는 방식
-//        guard let vc = storyboard?.instantiateViewController(identifier: "secondViewController") as? SecondViewController else { return }
+        let vc = storyboard?.instantiateViewController(identifier: "secondViewController") as! SecondViewController
+
+        vc.text = self.propertyTextField.text ?? ""
+        self.present(vc, animated: true)
+        
+//        self.navigationController?.pushViewController(vc, animated: true)
+//        present로 전달하는 방식
+//        let vc = storyboard?.instantiateViewController(withIdentifier: "secondViewController") as! SecondViewController
+//        let vc = SecondViewController(nibName:"SecondViewController", bundle: nil)
+//        let vc = SecondViewController()
 //
 //        vc.text = self.propertyTextField.text ?? ""
 //
-//        self.navigationController?.pushViewController(vc, animated: true)
-        //present로 전달하는 방식
-        let vc = storyboard?.instantiateViewController(withIdentifier: "secondViewController") as! SecondViewController
-       // let vc = SecondViewController(nibName:"SecondViewController", bundle: nil)
-//        let vc = SecondViewController()
-        
-        vc.text = self.propertyTextField.text ?? ""
-
-        self.present(vc, animated: true)
+//        self.present(vc, animated: true)
         
     }
     
